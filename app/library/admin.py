6884ab640d884842a9c710ddab92912eab763982
Ruslan Galimov <ruslan.galimov@corp.mail.ru>
2018-04-26 22:24:28 +0300

    add admin for categories

--- app/library/admin.py
+++ app/library/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from app.library.models import UserCategory
+from app.library.models import UserCategory, Category
 
 
 class UserCategoryAdmin(admin.ModelAdmin):
     pass
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(UserCategory, UserCategoryAdmin)
